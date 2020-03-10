94fd112810947df38df63ea281b83d3cf4c8e9ee
josemm83 <josemmancha@gmail.com>
2020-03-10 01:37:31 -0500

    updating files

--- server.js
+++ server.js
@@ -4,10 +4,19 @@ var path = require("path");
 var app = express();
 var PORT = process.env.PORT || 8080;
 
-app.use(express.static(__dirname, "/app/public"));
+app.use(express.urlencoded({extended: true}));
+app.use(express.json());
+app.use(express.static(__dirname + '/app/public'));
 
-require("./app/routing/apiRoutes");
-require("./app/routing/htmlRoutes");
+// app.get('/', function(req, res){
+//     res.sendFile(path.join(__dirname, "/app/public/home.html"));
+// });
+
+// app.get('/survey', function(req, res){
+// });
+
+require("./app/routing/apiRoutes")(app);
+require("./app/routing/htmlRoutes")(app);
 
 app.listen(PORT, function(){
     console.log("App listening on PORT " + PORT);
